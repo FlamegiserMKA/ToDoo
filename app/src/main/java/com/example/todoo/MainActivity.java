e4948962bf86852bb211b5c33cd15f990414a216
FlamegiserMKA <mkaflamegiser@gmail.com>
2023-05-08 00:23:30 +0530

    A sample task list has been created.

--- app/src/main/java/com/example/todoo/MainActivity.java
+++ app/src/main/java/com/example/todoo/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.todoo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.todoo.Adapter.ToDooAdapter;
+import com.example.todoo.Model.ToDooModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDooAdapter tasksadapter;
+    private List<ToDooModel>taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView=findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksadapter=new ToDooAdapter(this);
+        tasksRecyclerView.setAdapter(tasksadapter);
+
+        ToDooModel task = new ToDooModel();
+        task.setTask("Test Task");
+        task.setStatus(0);
+        task.setId(1);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksadapter.setTask(taskList);
     }
 }
